dae729fcaef1bdc4a4131ab1312bd26bdd18b593
dmitriish959 <80873437+dmitriish959@users.noreply.github.com>
2024-01-29 22:37:40 +0500

    fix

--- draft.py
+++ draft.py
@@ -1,27 +1,32 @@
-import requeset
-from relagam import Update
-from telegram.exe import Updater, CommandHandler, CallbackContext
-#Заменить "YOUR_TOKEN" на ваш токен от BotFather
+import requests
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Замените 'YOUR_TOKEN' на ваш токен от BotFather
 TOKEN = 'YOUR_TOKEN'
+
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я бот для поиска товаров на Ozon. Введите /search <товар>, что бы найти товар на Ozon.')
+    update.message.reply_text('Привет! Я бот для поиска товаров на Ozon. Введите /search <товар>, чтобы найти товар на Ozon.')
+
 def search(update: Update, context: CallbackContext) -> None:
-    query = ''. join(context.args)
-    response = requesets.get(f'https://api.ozon.ru/search/v2?keyword={query}')
+    query = ' '.join(context.args)
+    response = requests.get(f'https://api.ozon.ru/search/v2?keyword={query}')
     if response.status_code == 200:
         products = response.json()['result']['items']
-        for products in products:
+        for product in products:
             update.message.reply_text(f"{product['name']}: {product['price']} рублей")
-        else:
-            update.message.reply_text('Произошла ошибка при поиске товара')
+    else:
+        update.message.reply_text('Произошла ошибка при поиске товара.')
 
 def main() -> None:
-    update = Updater(TOKEN)
-    dispatcher = update.dispatcher
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
 
-    dispather.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('search', search))
+
     updater.start_polling()
     updater.idle()
+
 if __name__ == '__main__':
     main()
