609da2b03a5f6b667a04ece6f141b171dfee229a
dmitriish959 <80873437+dmitriish959@users.noreply.github.com>
2024-01-31 23:23:40 +0500

    Действительные числа Импорт библиотеки math - матанализа решение тестового задания

---
+++ parallel classes /действительные числа.py
@@ -0,0 +1,24 @@
+# Вызов библиотеки матанализа
+import math
+
+y = math.sin(x)
+print(math.sin(math.pi/2))
+
+
+# Использование функции из библиотеки math
+
+from math import*
+
+y = sin(x)
+print(sin(pi/2))
+
+
+
+# Дано положительное действительное число X. Выведите его дробную часть.
+import math
+
+x = float(input())
+print(x-int(x))
+
+
+# Ввод 17.9 ответ 0.899999999986
